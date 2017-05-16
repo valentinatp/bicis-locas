194f372e89fff33a8f8ccda92b06bf8cb143c7c3
valentinatp <valentina.toledoporma@gmail.com>
2017-05-16 00:35:55 -0400

    Creo una condicional para el nombre que retorna un 'span'

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@ function validateForm(){
 	var correo = document.getElementById("input-email").value;
 	var contra = document.getElementById("input-password").value;
 	var seleccion = document.getElementsByClassName("form-control").value;
+
+	if(nombre == "" || nombre.length == 0 || nombre.charAt(0) != nombre.charAt(0).toUpperCase()){
+			var span = document.createElement("span");
+			var ident = document.getElementById("name");
+			var padre = ident.parentNode;
+			padre.appendChild(span);
+
+			var contenido = document.createTextNode("Debe ingresar su nombre");
+			span.appendChild(contenido);
+		return span;
+	}	
+
 }
