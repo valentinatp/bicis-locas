1aade2f31832428e2eff11c907c4aa2dd7bbbf19
valentinatp <valentina.toledoporma@gmail.com>
2017-05-16 00:31:12 -0400

    Creo las variables llamando a los 'ids'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var correo = document.getElementById("input-email").value;
+	var contra = document.getElementById("input-password").value;
+	var seleccion = document.getElementsByClassName("form-control").value;
 }
